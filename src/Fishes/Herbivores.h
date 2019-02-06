7ee41cfc644c1b5e0c825172216a1b26793572d9
Ilya <victorieuse48@gmail.com>
2019-02-06 23:06:14 +0300

    Class realization has been implemented in source code.

--- src/Fishes/Herbivores.h
+++ src/Fishes/Herbivores.h
@@ -2,22 +2,23 @@
 #define AQUARIUM_HERBIVORES_H
 #pragma once
 
-
 #include "Creature.h"
 #include "Field.h"
+#include <random>
 
 class Herbivores : public Creature {
 public:
     Herbivores(int,int,Field*);
     bool stepForward() override;
     Creature* breedDescendant() override;
+    void move() override;
     ~Herbivores() = default;
 
-protected:
-    void move() override;
-
 private:
-    Field *field;
+    Creature* getClosest();
+    Creature* checkField(int,int);
+    void eat();
+    Field* field;
 };
 
 
